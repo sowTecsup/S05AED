f4ccc13113d5c5bdafc406ecec14e49fe674e8e4
sowTecsup <jneyra@tecsup.edu.pe>
2026-04-14 23:54:56 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+  
+
+}
